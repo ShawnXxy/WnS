b95aa7d4f2410ae69a0b18b1b8beb60401f2f0de
ShawnXxy <shawnxxy@hotmail.com>
2017-07-30 10:46:19 +0800

    initial commit

---
+++ src/UI/MainFrame.java
@@ -0,0 +1,19 @@
+package UI;
+
+import java.awt.Frame;
+
+public class MainFrame extends Frame {
+	
+	public MainFrame() {
+		// setting game window
+		this.setLocation(150, 50);
+		this.setSize(800, 600);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
